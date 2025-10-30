1f06fe576ce5c054b739f3b432074b6be2df27d2
Harish Kamuni <108679987+HarishKamuni@users.noreply.github.com>
2025-10-30 13:47:54 +0530

    created header and added google font

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="p-5 bg-primary flex justify-between">
+      <h1 className="text-secondary text-4xl font-semibold">H</h1>
+      <h1 className="text-white text-4xl font-semibold">R</h1>
+      <h1 className="text-tertiary text-4xl font-semibold">K</h1>
+    </div>
+  );
 };
 
 export default Header;
