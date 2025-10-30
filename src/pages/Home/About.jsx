154433eae068078a2e2befd55caecb62d429802d
Harish Kamuni <108679987+HarishKamuni@users.noreply.github.com>
2025-10-30 19:40:08 +0530

    completed About section

---
+++ src/pages/Home/About.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import SectionTitle from '../../components/SectionTitle';
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
+
+const About = () => {
+  return (
+    <div>
+      <SectionTitle title={'About'} />
+      <div className="flex items-center">
+        <div className="h-[70vh] w-4xl -ml-30">
+          <DotLottieReact
+            src="https://lottie.host/cee19b5c-a2f7-4aed-bb62-7b9b553e2265/egNbC0BVV3.lottie"
+            loop
+            autoplay
+          />
+        </div>
+        <div className="flex flex-col gap-5 w-1/2">
+          <p className="text-white text-sm font-light">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum
+            eveniet, culpa ad quaerat odio laboriosam earum perspiciatis error
+            obcaecati sapiente, doloremque velit suscipit sint voluptatem
+            expedita, cupiditate numquam reiciendis aliquid repudiandae ullam.
+            Distinctio a rerum error sed, laboriosam reprehenderit dolore.
+          </p>
+          <p className="text-white text-sm font-light">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui
+            molestiae modi alias nihil deleniti quisquam quam ipsam! Explicabo,
+            facere tempore placeat quia obcaecati ut nihil quaerat reiciendis
+            hic voluptas officiis quae, accusamus at a eius laborum. Quibusdam
+            doloremque architecto quas soluta nulla exercitationem, nam sed
+            excepturi officia, culpa itaque assumenda incidunt hic consequuntur
+            voluptate quod reiciendis non omnis perspiciatis illum.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
