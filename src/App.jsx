309147e6422f6a6290a5acdfc06def86a1e07137
Harish Kamuni <108679987+HarishKamuni@users.noreply.github.com>
2025-10-30 13:29:35 +0530

    install react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <>
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
