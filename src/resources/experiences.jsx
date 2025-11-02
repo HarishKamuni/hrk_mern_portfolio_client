f898dda096325ad1def05566d03de8e2d095b8d5
Harish Kamuni <108679987+HarishKamuni@users.noreply.github.com>
2025-11-02 19:00:30 +0530

    edit some static data

--- src/resources/experiences.jsx
+++ src/resources/experiences.jsx
@@ -1,6 +1,5 @@
 export const experiences = [
   {
-    _id: 1,
     company: 'Sheytech',
     title: 'React Developer',
     period: '2021-2022',
@@ -8,7 +7,6 @@ export const experiences = [
       'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam nemo accusantium illum quasi. Quae hic quia saepe quo. Quibusdam, molestiae?',
   },
   {
-    _id: 2,
     company: 'Infosis',
     title: 'React Developer',
     period: '2022-2023',
@@ -16,7 +14,6 @@ export const experiences = [
       'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam nemo accusantium illum quasi. Quae hic quia saepe quo. Quibusdam, molestiae?',
   },
   {
-    _id: 3,
     company: 'Pytm',
     title: 'React Developer',
     period: '2024-2025',
@@ -24,7 +21,6 @@ export const experiences = [
       'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam nemo accusantium illum quasi. Quae hic quia saepe quo. Quibusdam, molestiae?',
   },
   {
-    _id: 4,
     company: 'bookMyShow',
     title: 'React Developer',
     period: '2025-present',
