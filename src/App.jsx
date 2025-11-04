70e13906df4a3635b9f8916ec321e3337ab41756
Harish Kamuni <108679987+HarishKamuni@users.noreply.github.com>
2025-11-04 12:59:00 +0530

    connect the frontend with backend and get the profileData

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,20 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
 import Loader from './components/Loader';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
   const [shaowLoading, setShowLoading] = useState(false);
+  const getPortfolioData = async () => {
+    try {
+      const response = await axios.get('/api/portfolio/get-portfolio-data');
+      console.log(response);
+    } catch (error) {}
+  };
+  useEffect(() => {
+    getPortfolioData();
+  }, []);
   return (
     <BrowserRouter>
       {shaowLoading ? <Loader /> : null}
